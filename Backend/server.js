76b611edc8db8cff07f97dcd164498943bae7eac
Sonali Gupta <sonalibyahut0125@gmail.com>
2024-10-16 19:37:49 +0530

    server.js updated

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
+const centerRoutes = require('./routes/center');
+const sportRoutes = require('./routes/sport');
+const courtRoutes = require('./routes/court');
+const bookingRoutes = require('./routes/booking');
 
 const app = express();
 
@@ -20,6 +24,10 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/centers', centerRoutes);
+app.use('/api/sports', sportRoutes);
+app.use('/api/courts', courtRoutes);
+app.use('/api/bookings', bookingRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
